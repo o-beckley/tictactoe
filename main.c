0d03ea3963868051efcf759c9d90fc95378de100
Olasunkanmi Beckley <48617652+o-beckley@users.noreply.github.com>
2024-11-16 15:50:45 +0000

    Removed the bufferless input feature.

--- main.c
+++ main.c
@@ -2,24 +2,8 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include "tictactoe.h"
-#include <termio.h>
-
-void enable_bufferless_input() {
-    struct termios t;
-    tcgetattr(0, &t);
-    t.c_lflag &= ~(ICANON);
-    tcsetattr(0, TCSANOW, &t);
-}
-
-void disable_bufferless_input() {
-    struct termios t;
-    tcgetattr(0, &t);
-    t.c_lflag |= (ICANON);
-    tcsetattr(0, TCSANOW, &t);
-}
 
 void main(void) {
-    enable_bufferless_input();
     int c;
     int turn = 1;
     while(winner() == 0) {
@@ -28,6 +12,8 @@ void main(void) {
         print_board();
         printf("Player %c's turn: ", turn == 1 ? 'x' : 'o');
         c = getchar();
+        if(c == '\n')
+            continue;
         if(play_move(turn, c - '0'))
             turn = turn == 1 ? 2 : 1;
         printf("\n");
@@ -39,5 +25,4 @@ void main(void) {
         printf("Player x wins\n");
     else
         printf("Player o wins\n");
-    disable_bufferless_input();
 }
